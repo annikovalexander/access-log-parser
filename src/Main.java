0eff47bf1622281e3483e6384462ae6296d51a77
Alexander Annikov <aannikov@inno.tech>
2024-12-05 17:08:31 +0300

    com1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
          System.out.printf("Случайное число от 0 до 1: " + Math.random());
+         System.out.printf("Случайное число от 0 до 1: " + Math.random());
         }
     }
